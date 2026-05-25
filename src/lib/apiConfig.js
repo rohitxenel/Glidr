33f5d97e8409713a43b678da34b5e48bc5233ab4
rohitxenel <rohit.bamrara@xenelsoft.co.in>
2026-05-25 22:03:03 +0530

    comment

--- src/lib/apiConfig.js
+++ src/lib/apiConfig.js
@@ -3,3 +3,4 @@ export const API_BASE_URL = 'https://mobileappdevelopmentindia.com/ride/api/v1';
 export const IMAGE_URL = 'https://mobileappdevelopmentindia.com/ride/uploads/';
 // export const API_BASE_URL = 'http://192.168.0.109:3030/api/v1';
 // export const IMAGE_URL ="http://192.168.1.18:3030/uploads"
+//add 
